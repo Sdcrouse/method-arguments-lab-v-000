fecb62e873bcf85c3f068776b515b951b6b546ec
Steven Crouse <sdcrouse91@gmail.com>
2018-08-01 04:48:31 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
 def introduction name 
-  =begin 
+=begin 
   I won't do this in the future, but it's helpful to know that I can technically 
   write the method signature without enclosing the argument(s) between parentheses.
   Also, #print will not work since the test is expecting a new line. 
-  =end
+=end
   puts "Hi, my name is #{name}."
 end
 
