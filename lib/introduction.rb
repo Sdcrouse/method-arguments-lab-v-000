f7dec3309b8532480a0a9b0023a4c6673ec52dbb
Steven Crouse <sdcrouse91@gmail.com>
2018-08-01 04:43:18 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,6 @@
 def introduction name 
   # I won't do this in the future, but it's helpful to know that I can technically 
   # write the method signature without enclosing the argument(s) between parentheses.
-  print "Hi, my name is #{name}."
+  # Also, #print will not work since the test is expecting a new line. 
+  puts "Hi, my name is #{name}."
 end
