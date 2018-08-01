d3e2d9af996b0f936f8ba50c0aaa972203fba979
Steven Crouse <sdcrouse91@gmail.com>
2018-08-01 04:41:09 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,5 @@
-def introduction name
-  puts "Hi, my name is #{name}."
+def introduction name 
+  # I won't do this in the future, but it's helpful to know that I can technically 
+  # write the method signature without enclosing the argument(s) between parentheses.
+  print "Hi, my name is #{name}."
 end
